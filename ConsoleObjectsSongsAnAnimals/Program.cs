89f19a2dfb5c4bac745db92ac33dafcd3b51c8e7
Guy Siedes <3strategy@gmail.com>
2023-02-14 17:52:21 +0200

    Song

--- ConsoleObjectsSongsAnAnimals/Program.cs
+++ ConsoleObjectsSongsAnAnimals/Program.cs
@@ -4,7 +4,31 @@
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello, World!");
+      SongPorogram();
+    }
+
+    static void SongPorogram()
+    {
+      string[] strings = { "Gogo", "Guy", "Ziv", "Vienna", "Ten Times", "Aminem", "Mitgagea",
+            "Suiii","James","Elain","Obema","Lazy","Lame","Tahel","Tal",
+            "Nisim","Yariv","Jerry","Trump","Greg","Pingi","Nir",};
+
+      Random rnd = new Random();
+      Song[] songs = new Song[10];
+      for (int i = 0; i < songs.Length; i++)
+      {
+        Song s = new Song(strings[rnd.Next(0, 21)], rnd.Next(50, 250), strings[rnd.Next(0, 21)]);
+        songs[i] = s;
+      }
+      Console.WriteLine(AverageSongLength(songs));
+    }
+
+    static double AverageSongLength(Song[] sss)
+    {
+      double sum = 0;
+      foreach (var s in sss)
+        sum += s.GetLength();
+      return sum / sss.Length;
     }
   }
 }
---
+++ ConsoleObjectsSongsAnAnimals/Song.cs
@@ -0,0 +1,56 @@
+﻿using ConsoleObjectsSongsAnAnimals;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleObjectsSongsAnAnimals
+{
+  public class Song
+  {
+    private string name;
+    private string singer;
+    private int length;
+
+    public Song(string name, int length, string singer)
+    {
+      this.name = name;
+      this.length = length;
+      this.singer = singer;
+    }
+
+    public string GetName()
+    {
+      return name;
+    }
+    public void SetName(string value)
+    {
+      name = value;
+    }
+
+
+    public string GetSinger()
+    {
+      return singer;
+    }
+    public void SetSinger(string value)
+    {
+      singer = value;
+    }
+
+    public int GetLength()
+    {
+      return length;
+    }
+    public void SetLength(int value)
+    {
+      length = value;
+    }
+  }
+
+}
+
+
+
+
